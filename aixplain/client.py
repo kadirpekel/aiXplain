c58a47e030eb75095c0d6d560d1ce44ec57a96c2
Kadir Pekel <kadirpekel@gmail.com>
2023-08-11 00:41:49 +0300

    fixed docstrings

--- aixplain/client.py
+++ aixplain/client.py
@@ -12,7 +12,19 @@ DEFAULT_RETRY_STATUS_FORCELIST = [500, 502, 503, 504]
 
 def create_retry_session(total=None,
                          backoff_factor=None,
-                         status_forcelist=None):
+                         status_forcelist=None,
+                         **kwargs):
+    """
+    Creates a requests.Session with a specified retry strategy.
+
+    :param total: Total number of retries allowed (default is 5).
+    :param backoff_factor: Backoff factor to apply between retry attempts
+                           (default is 0.1).
+    :param status_forcelist: List of HTTP status codes to force a retry on
+                             (default is [500, 502, 503, 504]).
+    :param kwargs: Additional keyword arguments for internal Retry object
+    :return: A requests.Session object with the specified retry strategy.
+    """
     total = total or DEFAULT_RETRY_TOTAL
     backoff_factor = backoff_factor or DEFAULT_RETRY_BACKOFF_FACTOR
     status_forcelist = status_forcelist or DEFAULT_RETRY_STATUS_FORCELIST
@@ -20,6 +32,7 @@ def create_retry_session(total=None,
         total=total,
         backoff_factor=backoff_factor,
         status_forcelist=status_forcelist,
+        **kwargs
     )
     session = requests.Session()
     adapter = HTTPAdapter(max_retries=retry_strategy)
@@ -40,11 +53,17 @@ class AixplainClient:
         """
         Initialize AixplainClient with authentication and retry configuration.
 
-        :param base_url: The base URL for the API
-        :param aixplain_api_key: The individual API key
-        :param team_api_key: The team API key
-        :param max_retries: The maximum number of retries for a request
-        :param retry: Custom Retry configuration
+        :param base_url: The base URL for the API.
+        :param aixplain_api_key: The individual API key.
+        :param team_api_key: The team API key.
+        :param retry_total: Total number of retries allowed
+                            (default is None, uses DEFAULT_RETRY_TOTAL).
+        :param retry_backoff_factor: Backoff factor to apply between retry
+                                     attempts (default is None,
+                                     uses DEFAULT_RETRY_BACKOFF_FACTOR).
+        :param retry_status_forcelist: List of HTTP status codes to force a
+                                       retry on (default is None,
+                                       uses DEFAULT_RETRY_STATUS_FORCELIST).
         """
         self.base_url = base_url
         self.team_api_key = team_api_key
